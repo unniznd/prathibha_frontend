9f7846359836946fec276cfa8413e1ffbb798092
unniznd <aanandunnis@gmail.com>
2023-08-06 22:30:39 +0530

    unniku url added

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,8 +1,9 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
 
-const String baseURL = "https://api.prathibha.in";
+// const String baseURL = "https://api.prathibha.in";
 // const String baseURL = "http://localhost:8000";
+const String baseURL = "https://unniku.pythonanywhere.com";
 
 getToken() {
   final cookie = document.cookie!;
